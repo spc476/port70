ccb6253ae0aa5dde525926d3ab3820b4ccd871d7
Sean Conner <spc@conman.org>
2019-09-07 00:14:26 -0400

    Handle *.gopher files.
    These are my own gopher index files.  The format is:
    
    	|text what will be turned into an i type
    	|mroe text
    	|
    	|	file	selector	display
    	|	dir	selector	display
    	|	Lua	TBA
    	|	url	url		display
    	|
    	|text text text
    
    It's a nicer format than the usual gopher index file.  Those get the
    extension of .gophermap.

--- readfile.lua
+++ readfile.lua
@@ -23,12 +23,91 @@
 
 local mimetype = require "org.conman.parsers.mimetype"
 local magic    = require "org.conman.fsys.magic"
+local gtypes   = require "org.conman.const.gopher-types"
+local url      = require "org.conman.parsers.url.gopher"
+               + require "org.conman.parsers.url"
+local lpeg     = require "lpeg"
 local io       = require "io"
 local table    = require "table"
 
+local require  = require
+local tostring = tostring
+
 magic:flags('mime')
 
+-- ************************************************************************
+
+local parseline do
+  local Cc = lpeg.Cc
+  local C  = lpeg.C
+  local P  = lpeg.P
+  local R  = lpeg.R
+  
+  local entry = P"\t"
+              * C(R" ~"^1) * P"\t"^1 -- type
+              * C(R" ~"^1) * P"\t"^1 -- selector
+              * C(R" ~"^0)           -- display
+              
+  local info  = Cc"info"             -- type
+              * Cc""                 -- selector
+              * C(R" ~"^0)           -- display
+              
+  parseline = entry + info
+end
+
+-- ************************************************************************
+
 return function(filename)
+  if filename:match "%.gopher$" then
+    local CONF     = require "CONF"
+    local file,err = io.open(filename,"r")
+    if not file then
+      return false,err
+    end
+    
+    local acc = {}
+    for line in file:lines() do
+      local type,selector,display = parseline:match(line)
+      if type == 'url' then
+        local uri = url:match(selector)
+        if uri.scheme == 'gopher' then
+          table.insert(acc,string.format(
+                "%s%s\t%s\t%s\t%d",
+                gtypes[uri.type],
+                display,
+                uri.selector,
+                uri.host,
+                uri.port
+          ))
+        else
+          table.insert(acc,string.format(
+                "%s%s\t%s\t%s\t%d",
+                gtypes.html,
+                display,
+                "URL:" .. selector,
+                CONF.network.host,
+                CONF.network.port
+         ))
+        end
+      elseif type == 'Lua' then
+        -- XXX how to handle it
+        table.insert(acc,"iLUACODE\t-\t-\t0")
+      else
+        table.insert(acc,string.format(
+                "%s%s\t%s\t%s\t%d",
+                gtypes[type],
+                display,
+                selector,
+                CONF.network.host,
+                CONF.network.port
+        ))
+      end
+    end
+    
+    file:close()
+    return true,table.concat(acc,"\r\n") .. "\r\n.\r\n"
+  end
+  
   local mime = mimetype:match(magic(filename))
   
   if mime.type:match "^text/" then
@@ -41,6 +120,7 @@ return function(filename)
     for line in file:lines() do
       table.insert(acc,line)
     end
+    
     file:close()
     return true,table.concat(acc,"\r\n") .. "\r\n.\r\n"
     
