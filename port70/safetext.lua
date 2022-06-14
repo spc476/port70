6283cffa7c3b381e5e4d097ee52398dc92afb7e1
Sean Conner <spc@conman.org>
2022-06-13 20:21:59 -0400

    Remove utf8util module.
    I was only using one bit of it on one other location; the rest of the module
    went unused, so I decided to inline what I was using, and remove the rest of
    the module.

--- port70/safetext.lua
+++ port70/safetext.lua
@@ -27,13 +27,25 @@ local utf8    = require "org.conman.parsers.utf8.char"
 local control = require "org.conman.parsers.utf8.control"
               + require "org.conman.parsers.iso.control"
               + require "org.conman.parsers.ascii.control"
-local nc      = require "port70.utf8util".nc
 
 local Carg = lpeg.Carg
 local Cs   = lpeg.Cs
 local C    = lpeg.C
 local P    = lpeg.P
+local R    = lpeg.R
 
+local nc = P"\204"     * R"\128\191" -- combining chars
+         + P"\205"     * R"\128\175" -- combining chars
+         + P"\225\170" * R"\176\190" -- combining chars
+         + P"\225\183" * R"\128\191" -- combining chars
+         + P"\226\131" * R"\144\176" -- combining chars
+         + P"\239\184" * R"\160\175" -- combining chars
+         + P"\u{00AD}"               -- shy hyphen
+         + P"\u{1806}"               -- Mongolian TODO soft hyphen
+         + P"\u{200B}"               -- zero width space
+         + P"\u{200C}"               -- zero-width nonjoiner space
+         + P"\u{200D}"               -- zero-width joiner space
+         
 local c   = P"\9" * Carg(1) -- HT
           / function(s)
               local rem = 8 - (s.cnt % 8)
